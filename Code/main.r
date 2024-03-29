dc4b8dd2668cc236a995aa4f164718de8448db79
cokechi96 <chidubem_okechi@brown.edu>
2024-03-29 19:02:40 -0400

    included additional figures and regressions

--- Code/main.r
+++ Code/main.r
@@ -1,7 +1,8 @@
 ## Process raw data
-source("Code/merge_cleaned_data")
+source("Code/merge_cleaned_data.r")
 
 ## Make figures
-
+source("Code/make_figures.r")
 
 ## Run regressions
+source("Code/run_regressions.r")
--- Code/run_regressions.r
+++ Code/run_regressions.r
@@ -1,22 +1,34 @@
+library(tidyverse)
 library(fixest)
 library(modelsummary)
 library(gt)
 library(flextable)
 
-processed_data <- read.csv("Input/processed_data.csv")
+processed_data <- as_tibble(read.csv("Input/processed_data.csv"))
 
+## With Outliers
 ## Without IV
 model <- feols(quantity ~ price, data = processed_data)
-summary(model)
 
 ## WIth IV
 model_iv <- feols(quantity ~ 1 | price ~ legislation_count,
- data = processed_data)
-
+data = processed_data)
 
 modelsummary(list(model, model_iv), output = "Output/models.docx")
 modelsummary(list(model, model_iv), output = "Output/models.md")
 
-gt(model)
 
-#gof_map = c("nobs", "r.squared",
+## Without Outliers
+processed_data <- processed_data |>
+  filter(quantity <= 70000)|>
+  filter(quantity >= 30000)
+
+## Without IV
+NO_model <- feols(quantity ~ price, data = processed_data)
+
+## WIth IV
+NO_model_iv <- feols(quantity ~ 1 | price ~ legislation_count,
+data = processed_data)
+
+modelsummary(list(NO_model, NO_model_iv), output = "Output/NO_models.docx")
+modelsummary(list(NO_model, NO_model_iv), output = "Output/NO_models.md")
